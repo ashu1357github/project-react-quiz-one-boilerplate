e3bfc66b167a1fbefc8344976465f208a5355ade
ashu <ashupatil1357@gmail.com>
2024-05-07 15:11:02 +0530

    Done

---
+++ quiz/src/App.jsx
@@ -0,0 +1,19 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import HomeComponent from "./components/HomeComponent";
+import QuizComponent from "./components/QuizComponent";
+// import HomeComponent from "./components/HomeComponent";
+// import QuizComponent from "./components/QuizComponent";
+import ResultComponent from "./components/ResultComponent";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <HomeComponent></HomeComponent> */}
+      {/* <QuizComponent></QuizComponent> */}
+      <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
